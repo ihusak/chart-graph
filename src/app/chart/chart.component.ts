4dca5ebb7442fa4613ca40e9c021bd24d9d04e7a
Illia Husak <ilyagusak@gmail.com>
2019-10-20 21:09:24 +0300

    optimize

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import Chart from 'chart.js';
-import * as moment from 'moment';
 import { ChartService } from './chart.service';
 import { CoinInterface } from '../interfaces/coin.interface';
+import { makeGradient, generateTimeStamp, getOptions } from './helpers/functions';
 
 // default variables
 const DEFAULT_IDS: string = '7,10'; // example 2,4,10
@@ -32,9 +32,9 @@ export class ChartComponent implements OnInit {
    * @param coins
    */
   private buildGraph(coins): void {
-    const timeStamp = this.generateTimeStamp(coins[0].history.length);
+    const timeStamp = generateTimeStamp(coins[0].history.length);
     const data = this.generateData(coins);
-    const options = this.getOptions();
+    const options = getOptions(this.tooltipColors);
 
     this.chart = new Chart(this.chartRef.nativeElement, {
       type: 'line',
@@ -51,7 +51,7 @@ export class ChartComponent implements OnInit {
    */
   generateData(coins): [] {
     return coins.map((item) => {
-      const gradientItem = this.makeGradient(item.color);
+      const gradientItem = makeGradient(item.color, this.chartRef);
       // OLD REALIZATION, On my mind it's not perfect, I did my custom realization
       // const gradientItem = () => {
       //   const gradient = this.chartRef.nativeElement.getContext('2d').createLinearGradient(0, 0, 0, 650);
@@ -77,108 +77,5 @@ export class ChartComponent implements OnInit {
         data: item.history
       };
     });
-  }
-  /**
-   * Generate days from today
-   * @param count number
-   * @returns []
-   */
-  private generateTimeStamp(count): string[] {
-    const result = [];
-    for (let i = count; i >= 0; i -= 1) {
-      result.push(moment().subtract(i, 'd').format('MMM DD'));
-    }
-    return result;
-  }
-  /**
-   * Convert HEX to RGBA color
-   * @param hex
-   */
-  private hexToRgbA(hex): string {
-    let c;
-    if (/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)) {
-        c = hex.substring(1).split('');
-        if (c.length === 3) {
-            c = [c[0], c[0], c[1], c[1], c[2], c[2]];
-        }
-        c = '0x' + c.join('');
-        return 'rgba(' + [ (c >> 16) & 255, (c >> 8) & 255, c & 255].join(',') + ',1)';
-    }
-    throw new Error('Bad Hex');
-  }
-  /**
-   * Make custom gradient for graph
-   * @param colorHex
-   */
-  private makeGradient(colorHex): CanvasGradient {
-
-    const color = this.hexToRgbA(colorHex);
-    const changedColor = color.split(',');
-    const gradient = this.chartRef.nativeElement.getContext('2d').createLinearGradient(0, 0, 0, 650);
-
-    for (let i = 1; i >= 0; i -= 0.5) {
-      changedColor.splice(changedColor.length - 1, 1).push(`${i})`);
-      changedColor.push(`${1 - i})`);
-      gradient.addColorStop(i, changedColor.join(','));
-    }
-
-    return gradient;
-  }
-  /**
-   * Chart options
-   */
-  getOptions() {
-    return {
-      scales: {
-        yAxes: [{
-          gridLines : {
-            display : false
-            },
-            scaleLabel: {
-                  display: true,
-                  labelString: 'Currency $',
-                  fontSize: 20
-              }
-        }],
-        xAxes: [{
-          scaleLabel: {
-            display: true,
-            labelString: 'Date',
-            fontSize: 20
-        }
-        }]
-    },
-    tooltips: {
-      custom: (element) => {
-        let color;
-        if (element.dataPoints) {
-          color = this.tooltipColors[element.dataPoints[0].datasetIndex];
-        }
-        element.y -= 15;
-        element.backgroundColor = color;
-      },
-      callbacks: {
-        label: (item) => item.value,
-        title: () => ''
-      },
-      yAlign: 'bottom',
-      xAlign: 'center',
-      xPadding: 5,
-      yPadding: 15,
-      displayColors: false,
-      titleFontSize: 21
-    },
-    hover: {
-      mode: 'nearest',
-      onHover: function(e) {
-         const point = this.getElementAtEvent(e);
-         if (point.length) {
-           e.target.style.cursor = 'pointer';
-        } else {
-          e.target.style.cursor = 'default';
-        }
-      }
-   }
-    };
   }
 }
---
+++ src/app/chart/helpers/functions.ts
@@ -0,0 +1,110 @@
+import { ElementRef } from '@angular/core';
+import * as moment from 'moment';
+
+/**
+ * Convert HEX to RGBA color
+ * @param hex
+ */
+const hexToRgba = (hex): string => {
+ let c;
+ if (/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)) {
+     c = hex.substring(1).split('');
+     if (c.length === 3) {
+         c = [c[0], c[0], c[1], c[1], c[2], c[2]];
+     }
+     c = '0x' + c.join('');
+     return 'rgba(' + [ (c >> 16) & 255, (c >> 8) & 255, c & 255].join(',') + ',1)';
+ }
+ throw new Error('Bad Hex');
+};
+/**
+ * Make custom gradient for graph
+ * @param colorHex
+ */
+const makeGradient = (colorHex: string, ref: ElementRef): CanvasGradient => {
+
+    const color = hexToRgba(colorHex);
+    const changedColor = color.split(',');
+    const gradient = ref.nativeElement.getContext('2d').createLinearGradient(0, 0, 0, 650);
+
+    for (let i = 1; i >= 0; i -= 0.5) {
+      changedColor.splice(changedColor.length - 1, 1).push(`${i})`);
+      changedColor.push(`${1.2 - i})`);
+      console.log(changedColor, i);
+      gradient.addColorStop(i, changedColor.join(','));
+    }
+
+    return gradient;
+  };
+
+/**
+ * Generate days from today
+ * @param count number
+ * @returns []
+ */
+const generateTimeStamp = (count): string[] => {
+  const result = [];
+  for (let i = count; i >= 0; i -= 1) {
+    result.push(moment().subtract(i, 'd').format('MMM DD'));
+  }
+  return result;
+};
+/**
+ * Chart options
+ */
+const getOptions = (tooltipColors) => {
+  return {
+    scales: {
+      yAxes: [{
+        gridLines : {
+          display : false
+          },
+          scaleLabel: {
+                display: true,
+                labelString: 'Currency $',
+                fontSize: 20
+            }
+      }],
+      xAxes: [{
+        scaleLabel: {
+          display: true,
+          labelString: 'Date',
+          fontSize: 20
+      }
+      }]
+  },
+  tooltips: {
+    custom: (element) => {
+      let color;
+      if (element.dataPoints) {
+        color = tooltipColors[element.dataPoints[0].datasetIndex];
+      }
+      element.y -= 15;
+      element.backgroundColor = color;
+    },
+    callbacks: {
+      label: (item) => item.value,
+      title: () => ''
+    },
+    yAlign: 'bottom',
+    xAlign: 'center',
+    xPadding: 5,
+    yPadding: 15,
+    displayColors: false,
+    titleFontSize: 21
+  },
+  hover: {
+    mode: 'nearest',
+    onHover: function(e) {
+        const point = this.getElementAtEvent(e);
+        if (point.length) {
+          e.target.style.cursor = 'pointer';
+      } else {
+        e.target.style.cursor = 'default';
+      }
+    }
+  }
+  };
+}
+
+export {makeGradient, generateTimeStamp, getOptions}
